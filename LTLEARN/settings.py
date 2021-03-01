f119294322d4173f594e91df0f4b1c1f14d79ac3
ashwagzabani <ashwag.amz@gmail.com>
2021-03-01 23:55:37 +0300

    fix some issue and add forms python file

--- LTLEARN/settings.py
+++ LTLEARN/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fergeoi0vojb0ww8!jx%tr*izse8t18o%u&ak^(0(y&c#++!i)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'LTLEARN.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'platform_app',
+        'PASSWORD': os.environ.get('password'),
+        'USER': 'postgres'
     }
 }
 
